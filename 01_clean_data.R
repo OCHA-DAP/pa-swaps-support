1c4118906c6ca612895fe1e9a2dd19f5f021f12a
Seth Caldwell <caldwellst@gmail.com>
2023-06-07 11:07:54 +0100

    Setup the repo

---
+++ 01_clean_data.R
@@ -0,0 +1,392 @@
+library(tidyverse)
+library(jsonlite)
+
+input_dir <- file.path(
+  Sys.getenv("SWAPS_SUPPORT"),
+  "input"
+)
+
+output_dir <- file.path(
+  Sys.getenv("SWAPS_SUPPORT"),
+  "output"
+)
+
+# helper function to format worksheets
+# use integers to specify columns
+# use for single column
+format_2022_col <- function(wb, sheet, rows, col) {
+  # get reference for 1st previous column and starting row
+  prev_col <- int2col(col - 1)
+  curr_col <- int2col(col)
+  start_row <- rows[1]
+  rule <- paste0(
+    curr_col,
+    start_row,
+    "!=",
+    prev_col,
+    start_row
+  )
+
+  conditionalFormatting(
+    wb = wb,
+    sheet = sheet,
+    cols = col,
+    rows = rows,
+    rule = rule,
+    type = "expression"
+  )
+}
+
+# applies formatting across multiple columns
+# has to be done separately
+format_2022_cols <- function(wb, sheet, rows, cols) {
+  walk(
+    .x = cols,
+    .f = \(col) {
+      format_2022_col(
+        wb = wb,
+        sheet = sheet,
+        rows = rows,
+        col = col
+      )
+    }
+  )
+}
+
+# create sheet, write data and format at the same time
+# assume data has column names and first row of years
+write_swaps_data <- function(wb, sheet, df) {
+  # get dimensions of the data frame
+  years <- unlist(df[1,], use.names = FALSE)
+  cols <- which(years == 2022)
+  rows <- 3:(nrow(df) + 1) # need to account for adding headers as rows
+
+  addWorksheet(wb, sheet)
+  writeData(wb, sheet, df)
+  format_2022_cols(wb, sheet, rows, cols)
+}
+
+
+##########################
+#### LOAD INPUT JSONS ####
+##########################
+
+df_cluster <- read_json(
+  file.path(
+    input_dir,
+    "CDM Survey Cluster Data extract 230601.json"
+  ),
+  simplifyVector = TRUE
+) %>%
+  pluck(1) %>%
+  as_tibble()
+
+df_iccg <- read_json(
+  file.path(
+    input_dir,
+    "CDM Survey HCT-ICCG Data extract 230601.json"
+  ),
+  simplifyVector = TRUE
+) %>%
+  pluck(1) %>%
+  as_tibble() %>%
+  mutate(
+    year = 2019 + reportingWindowId,
+  )
+
+###################
+#### ICCG DATA ####
+###################
+
+# ICCG data has a single response for each operation, so use that as the
+# unique identifier, we deal with repeat groups separately
+
+wb_iccg <- createWorkbook()
+
+df_iccg_wide <- df_iccg %>%
+  select(
+    IN_Operation,
+    everything(),
+    -reportingWindowId,
+    -where(is.list)
+  ) %>%
+  pivot_wider(
+    id_cols = IN_Operation,
+    names_from = year,
+    values_from = submissionId:HCT_CashIncreaseNo,
+    names_glue = "{.value}_{year}"
+  ) %>%
+  mutate(
+    across(
+      .cols = everything(),
+      .fns = as.character
+    )
+  )
+
+# create secondary row with year to prepare for notebooking for Excel
+iccg_years <- str_extract(names(df_iccg_wide), "([0-9]{4}$)")
+iccg_names <- names(df_iccg_wide)
+names(iccg_years) <- iccg_names
+df_iccg_wb <- add_row(df_iccg_wide, !!!iccg_years, .before = 1)
+names(df_iccg_wb) <- str_remove(iccg_names, "_[0-9]{4}$")
+
+write_swaps_data(
+  wb = wb_iccg,
+  sheet = "ICCG",
+  df = df_iccg_wb
+)
+
+############################
+#### ICCG SUBGROUP: HCT ####
+############################
+
+# ICCG data that is in repeat groups are held separately from the main data
+# need to explore that separately for cleaning
+# for now doing these separately to
+
+df_hct <- df_iccg %>%
+  select(
+    IN_Operation,
+    submissionId,
+    year,
+    HCTOrg
+  ) %>%
+  unnest(
+    HCTOrg
+  ) %>%
+  rename_with(
+    .fn = \(x) str_remove(x, "^HCT_Org"),
+    .cols = everything()
+  ) %>%
+  select(
+    -starts_with("Att") # not collected in 2022
+  ) %>%
+  type_convert() %>%
+  mutate(
+    across(
+      .cols = c(TypeINGO, TypeNNGO),
+      .fns = \(x) replace_na(x, 0)
+    )
+  )
+
+# do a simple match of 2022 data with 2021 to highlight when something was
+# present in 2022 but not in 2021 and vice versa
+
+df_hct_2021 <- filter(df_hct, year == 2021)
+df_hct_2022 <- filter(df_hct, year == 2022)
+
+join_cols <- c("IN_Operation", "Type", "Name", "Status", "NameOther", "StatusOther", "TypeINGO", "TypeNNGO")
+
+df_hct_wb <- left_join(
+  x = df_hct_2022,
+  y = df_hct_2021 %>%
+    mutate(in_2021 = TRUE) %>%
+    select(-year, -submissionId, -Calc),
+  by = join_cols,
+  relationship = "many-to-many"
+) %>%
+  mutate(
+    in_2021 = replace_na(in_2021, FALSE)
+  ) %>%
+  bind_rows(
+    anti_join(
+      df_hct_2021,
+      df_hct_2022,
+      by = join_cols
+    ) %>%
+      mutate(
+        year = 2021,
+        in_2022 = FALSE
+      )
+  ) %>%
+  group_by(
+    year,
+    IN_Operation,
+    Type,
+    Name,
+    Status,
+    NameOther,
+    StatusOther,
+    TypeINGO,
+    TypeNNGO
+  ) %>%
+  mutate(
+    in_2022 = replace_na(in_2022, TRUE),
+    year_check = case_when(
+      !in_2021 ~ "new addition in 2022",
+      !in_2022 ~ "missing from 2022",
+      TRUE ~ ""
+    ),
+    duplicate_check = ifelse(
+      n() == 1 | year == 2021,
+      "",
+      "multiple rows for 2022"
+    )
+  ) %>%
+  select(
+    -starts_with("in_2")
+  ) %>%
+  arrange(
+    IN_Operation,
+    desc(year),
+    Type,
+    Name
+  )
+
+# save to a workbook
+
+addWorksheet(
+  wb_iccg,
+  "HCTOrg"
+)
+
+writeData(
+  wb_iccg,
+  "HCTOrg",
+  df_hct_wb
+)
+
+conditionalFormatting(
+  wb = wb_iccg,
+  sheet = "HCTOrg",
+  rows = 1:nrow(df_hct_wb) + 1,
+  cols = 12:13,
+  rule = 'J2<>""'
+)
+
+saveWorkbook(
+  wb = wb_iccg,
+  file = file.path(output_dir, "swaps_iccg_data.xlsx"),
+  overwrite = TRUE
+)
+
+##################################
+#### ICCG SUBGROUP: LOCSubnat ####
+##################################
+
+# not covered because no data from 2022
+
+##################################
+#### ICCG SUBGROUP: SUBGroups ####
+##################################
+
+df_subgroup_chairs <- df_iccg %>%
+  select(
+    IN_Operation,
+    submissionId,
+    year,
+    SUBGroups
+  ) %>%
+  unnest(
+    SUBGroups
+  ) %>%
+  rename_with(
+    .fn = \(x) str_remove(x, "^SUB_"),
+    .cols = everything()
+  ) %>%
+  type_convert() %>%
+  select(
+    -contains("Chairs") # only used in 2020
+  ) %>%
+  pivot_longer(
+    cols = matches("^Chair[0-9]{1}"),
+    names_to = c("ChairNumber", "name"),
+    names_pattern = "Chair([0-9]{1})(.*)"
+  ) %>%
+  pivot_wider(
+    names_prefix = "Chair"
+  ) %>%
+  filter(
+    year != "2020",
+    !is.na(ChairType)
+  ) %>%
+  select(
+    IN_Operation,
+    year,
+    submissionId,
+    NumCalc,
+    Theme,
+    ThemeOther,
+    Report,
+    ReportOther,
+    SecType,
+    SecName,
+    SecNameOther,
+    ChairFullTime,
+    ChairNumber,
+    ChairType,
+    ChairName,
+    ChairNameOther
+  )
+
+df_subgroups_2021 <- filter(df_subgroups, year == 2021)
+df_subgroups_2022 <- filter(df_subgroups, year == 2022)
+
+
+# do some comparisons between 2022 and 2021
+# looking at first at the general subgroups
+
+df_subgroups_wb <- df_subgroups_2022 %>%
+  left_join(
+    df_subgroups_2021 %>%
+      distinct(
+        IN_Operation,
+        Theme,
+        Report,
+        ReportOther
+      ) %>% mutate(
+      group_existed_2021 = TRUE
+    ),
+    by = c("IN_Operation", "Theme", "Report", "ReportOther"),
+    relationship = "many-to-many"
+  ) %>%
+  left_join(
+    df_subgroups_2021 %>%
+      select(
+        IN_Operation,
+        Theme,
+        Report,
+        ReportOther,
+        ChairType,
+        ChairName,
+        ChairNameOther
+      ) %>%
+      mutate(
+        chair_existed_2021 = TRUE
+      ),
+    relationship = "many-to-many",
+    by = c("IN_Operation", "Theme", "Report", "ReportOther", "ChairType", "ChairName", "ChairNameOther")
+  ) %>%
+  bind_rows(
+    anti_join(
+      df_subgroups_2021,
+      df_subgroups_2022,
+      by = c("IN_Operation", "Theme", "Report", "ReportOther")
+    ) %>%
+      mutate(
+        dropped_2022 = TRUE
+      )
+  ) %>%
+  mutate(
+    subgroup_check = case_when(
+      dropped_2022 ~ "Subgroup was not listed in 2022",
+      !group_existed_2021 ~ "Subgroup is new in 2022",
+      TRUE ~ ""
+    ),
+    chair_check = ifelse(
+      year == 2022
+    )
+  )
+
+
+##################################
+#### ICCG SUBGROUP: FRMRRMOrg ####
+##################################
+
+#####################################
+#### ICCG SUBGROUP: HCTSubnatLoc ####
+#####################################
+
+#####################################
+#### ICCG SUBGROUP: ICCSubnatLoc ####
+#####################################
