800face7bba8abd1ed9edfe7b6cf31bd17e6a5be
Zack  Arno <zackarno@gmail.com>
2023-06-14 12:34:08 +0300

    add convenience function for reading all or multiple tabs of excel wb

--- clean_data/utils.R
+++ clean_data/utils.R
@@ -78,3 +78,33 @@ write_swaps_yearly_data <- function(wb, sheet, df) {
   format_2022_cols(wb, sheet, rows, cols)
 }
 
+
+
+
+#' read_all_tabs from excel file
+#'
+#' @param fp \code{character} file path
+#' @param skip \code{numeric} Minimum number of rows to skip before reading anything, be it column names or data. Leading empty rows are automatically skipped, so this is a lower bound. Ignored if range is given.
+#'
+#' @return list of data.frames
+#'
+
+read_all_tabs <- function(fp,clean_names=T,skip=1,col_names=T, sheet_names=NULL){
+  if(is.null(sheet_names)){
+    sheet_names <- readxl::excel_sheets(fp)
+  }
+  sheet_names %>%
+    purrr::map(
+      \(x){
+        ret <- readxl::read_xlsx(path = fp,sheet = x,skip=skip,col_names = col_names)
+        if(clean_names){
+          ret <- ret %>% janitor::clean_names()
+        }
+        return(ret)
+
+      }
+    ) %>%
+    set_names(sheet_names)
+}
+
+
